be9c574bfc90637b58e81971503938a7cbb121ad
dan <dnlrbns@users.noreply.github.com>
2019-11-02 21:13:31 +1100

    update to correct* model
    *who knows

--- integrate_and_plot.py
+++ integrate_and_plot.py
@@ -2,35 +2,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-m_a = 0.1
-m_d = 0.5
-r = 0.05
+m1= 0.1
+m2 = 0.1
+r = 0.04
 l1 = 0.1
 l2 = 0.2
-I_a = (1/12) * m_a * l1**2
-I_d = (1/2)  * m_d * r**2
-b = 0.001
-c = 0.01
+I1 = (1/12) * m1 * l1**2
+I2 = (1/2)  * m2 * r**2
+b = 0.01
+c = 0.001
 T=0
 g=9.8
-ref = 0
+ref = -np.pi/2
 
 def dZ_dt(t, Z):
     dZ = [None, None, None, None, None, None]
     dZ[0] = Z[1]
-    n1 = (m_a*g*l1 - m_d*g*l2)*np.cos(Z[0]) - b*Z[1] + c*Z[3] - T
-    d1 = m_a*l1 + m_d*l2 + I_a
+    n1 = (m1*g*l1 - m2*g*l2)*np.cos(Z[0]) \
+        - b*Z[1] \
+        + c*Z[3] \
+        - T
+    d1 = m1*l1**2 + m2*l2**2 + I1
     dZ[1] = n1/d1
     dZ[2] = Z[3]
-    n2 = T - c*Z[3] - dZ[1]*I_d + m_d*g*l2*np.cos(Z[0])
-    d2 = I_d
+    n2 = T \
+        - c*Z[3] \
+        - dZ[1]*I2
+    d2 = I2
     dZ[3] = n2/d2
     dZ[4] = ref - Z[0]
     dZ[5] = -Z[1]
-    dZ = [None, None, None, None, None, None]
     return dZ
 
-sol = solve_ivp(dZ_dt, [0,10], [-np.pi/2,0,0,0,0,0])
+sol = solve_ivp(dZ_dt, [0,4], [-np.pi/2,0,0,20*np.pi,ref+np.pi/2,0], max_step=.05)
 
-plt.plot(sol.t,np.rad2deg(np.transpose(sol.y)))
+try:
+    plt.close('all')
+except:
+    print('no figures to close')
+
+fig,[ax0,ax1,ax2,ax3]= plt.subplots(4,1,sharex=True)
+l0, = ax0.plot(sol.t,np.rad2deg(sol.y[[0],:].T))
+ax0.grid()
+l1, = ax1.plot(sol.t,sol.y[[1],:].T/2/np.pi)
+ax1.grid()
+l2, = ax2.plot(sol.t,sol.y[[3],:].T/2/np.pi)
+ax2.grid()
+l3, = ax3.plot(sol.t,np.rad2deg(sol.y[[4],:].T))
+ax3.grid()
+fig.legend()
 plt.show()
